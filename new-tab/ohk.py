fc8d2d38ae416a9846ff27dd4b9865fcf85ef3fe
Prabesh Sapkota <prabesh01@pm.me>
2023-10-18 15:49:30 +0545

    Update ohk.py

--- new-tab/ohk.py
+++ new-tab/ohk.py
@@ -11,7 +11,7 @@ for song in songs:
     if i==0:
         i=1
     else:
-        data1+=f'\n<img class="img slider__img" src="https://i.ytimg.com/vi/{songs[song][3]}/default.jpg" alt="cover">'
+        data1+=f'\n<img class="img slider__img" src="https://i.ytimg.com/vi/{songs[song][3]}/hqdefault.jpg" alt="cover">'
 
     data2+=f'\n<li class="player__song"><img class="player__img img" src="https://i.ytimg.com/vi/{songs[song][3]}/default.jpg" alt="cover"><p class="player__context "><b class="player__song-name"> {songs[song][0]}</b><span class="flex"><span class="player__title">{songs[song][1]}</span><span class="player__song-time"></span></span></p><audio class="audio" src="https://f003.backblazeb2.com/file/mah-music/songs/{song}.{songs[song][2]}" preload="none" crossorigin="anonymous" spotify="{songs[song][4]}" youtube="{songs[song][3]}"></audio></li>'
     
